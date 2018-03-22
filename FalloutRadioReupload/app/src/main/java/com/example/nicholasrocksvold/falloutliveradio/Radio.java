6e7545f786ae2927bbea5d79f242cbe8ae088b03
minx112 <grant.sipes821@myci.csuci.edu>
2018-03-21 18:40:37 -0700

    working on Radio object
    not a working app

--- FalloutRadioReupload/app/src/main/java/com/example/nicholasrocksvold/falloutliveradio/Radio.java
+++ FalloutRadioReupload/app/src/main/java/com/example/nicholasrocksvold/falloutliveradio/Radio.java
@@ -1,22 +1,30 @@
 package com.example.nicholasrocksvold.falloutliveradio;
 
+import android.content.Context;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Radio {
 
     //Radio class holds the Name and array of songs
     public String radioName;
     public ArrayList<SoundObject> songs;
+    private Context context;
 
-    public Radio(String radioName, int start, int end)
+    public Radio(Context current, String radioName, int start, int end)
     {
         int range = end - start;
 
         this.radioName = radioName;
+        this.context = current;
+
+        List<String> nameList = Arrays.asList(context.getResources().getStringArray(R.array.soundNames));
 
         for(int i = 0; i < range; i++)
         {
-            songs.add(new SoundObject(nameList))
+            songs.add(new SoundObject(nameList.get(start + 0), R.raw.audio1))
         }
     }
 
