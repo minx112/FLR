c6af2ce3b24d60aa644b11a0d1d820be408ffe8b
minx112 <grant.sipes821@myci.csuci.edu>
2018-03-22 18:03:01 -0700

    playing audio
    currently will only play first song

--- FalloutRadioReupload/app/src/main/java/com/example/nicholasrocksvold/falloutliveradio/RadioMenu.java
+++ FalloutRadioReupload/app/src/main/java/com/example/nicholasrocksvold/falloutliveradio/RadioMenu.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class RadioMenu extends AppCompatActivity {
 
     private Button mPlayButton;
@@ -16,18 +18,22 @@ public class RadioMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio);
 
-        mPlayButton = (Button) findViewById(R.id.play_button);
+        final int[] GNRSongs = new int[]{R.raw.song1, R.raw.song2, R.raw.song3, R.raw.song4, R.raw.song5,
+                R.raw.song6, R.raw.song7, R.raw.song8, R.raw.song9, R.raw.song10, R.raw.song11,
+                R.raw.song12, R.raw.song13, R.raw.song14, R.raw.song15, R.raw.song16, R.raw.song17,
+                R.raw.song18, R.raw.song19, R.raw.song20};
+
+                mPlayButton = (Button) findViewById(R.id.play_button);
 
         mPlayButton.setText(R.string.play_button);
-
-        final Radio GNR = new Radio(this, "Galaxy News Radio", 1, 20);
 
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 while(true)
                 {
-                    mp = GNR.songs.get(0);
+                    mp = MediaPlayer.create(getApplicationContext(), GNRSongs[0]);
+                    mp.start();
                 }
 
             }
