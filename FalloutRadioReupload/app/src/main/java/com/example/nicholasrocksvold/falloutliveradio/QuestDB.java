97b722ac09aaa9fe5fc90e18bcae6fa36a766c03
Mitchell Dennen <mitchelldennen@aol.com>
2018-05-09 16:27:05 -0700

    newest

--- FalloutRadioReupload/app/src/main/java/com/example/nicholasrocksvold/falloutliveradio/QuestDB.java
+++ FalloutRadioReupload/app/src/main/java/com/example/nicholasrocksvold/falloutliveradio/QuestDB.java
@@ -11,8 +11,8 @@ public class QuestDB {
     public static final class QuestTable {
         public static final String NAME = "quests";
         public static final class Cols {
-            public static final String currentQuest = "currentquest";
-            public static final String timeClosed = "timeclosed";// date convert to date
+            public static final Integer currentQuest = 0;
+            public static final Integer timeClosed = 0;// date convert to date
             public static final String questsDone = "questsdone";// make string, seperate "ints" by coma, parse string to get sub strings of quests done
             public static final Integer currentQuestTime = 0; // keep track in miliseconds
             public static final String gender = "gender";
